2ab7b204cabf9b61ae69633a0dd26b73a0544b23
Edu-Hiroshi <eduhito57@gmail.com>
2022-04-26 13:51:09 -0300

    added all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
+import ResultList from './components/ResultList';
+import FloatingCard from './components/FloatingCard';
 
 function App() {
+  const [descricao, setDescricao] = useState('pesquisa');
+  const [pesquisa, setPesquisa] = useState();
+
+  useEffect(() => {
+    alert('Atualizacao!');
+  }, [descricao, pesquisa]) // como é um array, podem ter main
+
+  const handleClick = useCallback(() => {
+    console.log ('OK - clicado');
+
+    setDescricao ('pesquisado');
+  }, [setDescricao]);
+
+  const handleChange = useCallback ((event) => {
+    console.log ('OK - pesquisado');
+    
+    setPesquisa (event);
+    console.log(event.target.value);
+  }, []);
+
+  // input com a condicao onChange{handleChange}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {descricao}
+        <button onClick={handleClick}>
+          pesquisar
+        </button>
+        <input onChange={handleChange}>
+          local de pesquisa
+        </input>
+        
+      </div>
+
+      <div style={{ display: 'grid', gridTemplateRows: '1fr auto'}}>
+        <div>
+          <div>Localização</div>
+          <div>Safe</div>
+          <div>Time</div>
+        </div>
+        
+        <ResultList/>
+        
+        <div style={{ gridRow: 2, gridColumn: 3}}>
+          <FloatingCard />
+        </div>
+
+      </div>
+
     </div>
+
   );
-}
+  
+};
+  
 
 export default App;
---
+++ src/components/FloatingCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const cardStyle = {
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  borderRadius: '10px',
+  borderColor: 'rgba(0,0,0,.5)',
+  padding: '10px',
+};
+
+const FloatingCard = () => {
+  <div>
+    <h1>Titulo</h1>
+    <p>
+      Teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeexto
+    </p>
+    <div>Imagem</div>
+  </div>
+};
+
+export default FloatingCard;
